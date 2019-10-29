52d58dfe5a6369951daa79ac7eb5ebd2253a7a10
Jonny H <jonathan.harrison@hotmail.co.uk>
2019-10-29 13:23:51 +0000

    Added div

---
+++ Practical5_PHPApplication/public/index.php
@@ -0,0 +1,12 @@
+<?php
+$footer = include_once 'footer.php';
+$header = include_once 'header.php';
+?>
+
+<body class ="bg-info">
+<div class="container-fluid col-md-10 offset-md-1">
+    <div class="row">
+        <div class="card mt-3 px-2 py-2">
+            <h1 "Stationery Application"</h1>
+        </div>
+
