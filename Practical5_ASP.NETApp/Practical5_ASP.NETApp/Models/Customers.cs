228567ec860a423ee6768d1f1ea18fd3536b080a
Jonathan Harrison <44781050+jharrison00@users.noreply.github.com>
2019-11-02 18:26:39 +0000

    Added database first application using ASP.NET

---
+++ Practical5_ASP.NETApp/Practical5_ASP.NETApp/Models/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical5_ASP.NETApp.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int CustomerId { get; set; }
+
+        public ICollection<Orders> Orders { get; set; }
+    }
+}
