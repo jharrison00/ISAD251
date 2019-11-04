d9baa2e4cc43a3f762201b81071df7e4426e2376
Jonathan Harrison <jharrison12@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-04 14:57:32 +0000

    Added bootstrap

---
+++ Practical5_ASP.NET(BootStrap)/Practical5_ASP.NETApp/Models/ProductsAudit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical5_ASP.NETApp.Models
+{
+    public partial class ProductsAudit
+    {
+        public int AuditId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductDetails { get; set; }
+        public double OldPrice { get; set; }
+        public double NewPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateChanged { get; set; }
+    }
+}
