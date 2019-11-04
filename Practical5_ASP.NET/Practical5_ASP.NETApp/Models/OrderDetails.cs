3b8fd60951b915e0cab0436d9d24fd546aaa3c0c
Jonathan Harrison <jharrison12@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-04 15:01:23 +0000

    Database first workflow in ASP.NET

---
+++ Practical5_ASP.NET/Practical5_ASP.NETApp/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical5_ASP.NETApp.Models
+{
+    public partial class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal? LineTotal { get; set; }
+
+        public Products Product { get; set; }
+    }
+}
---
+++ Practical5_ASP.NET/Practical5_ASP.NETApp/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical5_ASP.NETApp.Models
+{
+    public partial class Products
+    {
+        public Products()
+        {
+            OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductDetails { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int? Reordering { get; set; }
+        public int? MinimumStock { get; set; }
+
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
