097cf5ad2b28e26c0318459ca3b9df6b32176199
Jonathan Harrison <jharrison12@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-04 15:02:21 +0000

    Added bootstrap design to webpage

---
+++ Practical6_ASP.NET/Practical5_ASP.NETApp/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical5_ASP.NETApp.Models
+{
+    public partial class Orders
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+
+        public Customers Customer { get; set; }
+    }
+}
